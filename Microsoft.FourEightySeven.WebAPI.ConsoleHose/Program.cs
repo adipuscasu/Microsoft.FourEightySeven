5961435e39bf797796783ed6acec10b5a5d42fb4
Adrian Puscasu <apu@f5it.ro>
2019-02-23 19:46:32 +0200

    - adds web publish profile, signing the assembly;

--- Microsoft.FourEightySeven.WebAPI.ConsoleHose/Program.cs
+++ Microsoft.FourEightySeven.WebAPI.ConsoleHose/Program.cs
@@ -2,22 +2,23 @@
 using System.Web.Http;
 using System.Web.Http.SelfHost;
 
-namespace Microsoft.FourEightySeven.WebAPI.ConsoleHose
+namespace Microsoft.FourEightySeven.WebAPI.ConsoleHost
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Starting Web API Server. Please wait...");
-            if (typeof(Controllers.CustomersController) == null)
+            var type = typeof(Microsoft.FourEightySeven.WebAPI.Controllers.HomeController);
+            if (type == null)
             {
                 // work-around
                 return;
             }
-            var hostConfig = new HttpSelfHostConfiguration("http://localhost:8080");
+            var hostConfig = new HttpSelfHostConfiguration("http://localhost:8081");
             hostConfig.Routes.MapHttpRoute("API Name",
                 " {controller}/{action}/{id}",
-                new { id = RouteParameter.Optional });
+                new {controller = "Home", action = "Index",  id = RouteParameter.Optional });
             using (HttpSelfHostServer server = new HttpSelfHostServer(hostConfig))
             {
                 server.OpenAsync().Wait();
